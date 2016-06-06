bc8bfba15d90d432f50a7b0448e3b497acd498e3
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-06-06 16:08:32 +0100

    Week-Three: HTTP & Sinatra

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World - I am cameron's new website."
+end
+
+get '/secret' do 
+  "Shhhh this part of the site is secret!"
+end
+
+get '/home' do 
+  "Welcome to the homepage"
+end
+
+get '/lemons' do 
+  "LEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONSLEMONS"
+end
+
+get '/hassan' do
+  "HEY HASS-MAN"
+end
+
+get '/cat' do
+  "<div>
+  <img src= 'http://bit.ly/1eze8aE'>
+  <div>"
+end
