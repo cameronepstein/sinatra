1e05c860ffe4cd88f00a58f81103df4d4d74fcfe
Graham Ormiston <grahamormiston@gmail.com>
2016-06-07 14:05:00 +0100

    #12: Sinatra introducing params

--- app.rb
+++ app.rb
@@ -16,7 +16,13 @@ get '/golly' do
 	"I can't believe it"
 end
 
-get '/cat' do
+get '/random-cat' do
   @names = ["amigo", "oscar", "viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @names = params[:name]
   erb(:index)
 end
